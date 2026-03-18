49e2cf3dd6a952172b13ebea5bbe88acf87e22be
rtait77 <rebeccatait77@gmail.com>
2026-03-17 22:04:53 -0400

    Add Next.js wrapper for Blast the Dwarf Planets easy

---
+++ app/asteroid-blast-dwarf-planets-easy/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useEffect } from 'react'
+export default function AsteroidBlastDwarfPlanetsEasy() {
+  useEffect(() => {
+    const prev = document.body.style.overflow
+    document.body.style.overflow = 'hidden'
+    return () => { document.body.style.overflow = prev }
+  }, [])
+  return (
+    <iframe src="/asteroid-blast-dwarf-planets-easy.html"
+      style={{ position:'fixed', inset:0, width:'100%', height:'100%', border:'none', zIndex:100 }} />
+  )
+}
